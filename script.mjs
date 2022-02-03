c5e03744592f813415bbfbbc98ccfe0d95162661
Ivan Zaplatar <64557487+ivzap@users.noreply.github.com>
2022-02-03 13:54:28 -0600

    Added basic functions for user data

---
+++ script.mjs
@@ -0,0 +1,65 @@
+import fetch from 'node-fetch';
+import getToken from './secrets.mjs';
+let oAuth = getToken();
+let defaultPayload = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer "+ oAuth
+  }
+};
+
+async function spotifyApiRequest(url, payload){
+  let response = await fetch(url, payload);
+  response = await response.json();
+  return response;  // Note: must use await on function call 
+}
+
+async function getUserPlaylists(user, payload = defaultPayload) {
+  let playlists = [];
+  let url = `https://api.spotify.com/v1/users/${user}/playlists`
+  let response = await spotifyApiRequest(url, payload);
+  response = response['items'];
+  for(const playlist of response){
+    playlists.push(playlist['name']);
+  }
+  return playlists; // Note: must use await on function call 
+}
+
+async function getPlaylistTracks(playlist_id, payload = defaultPayload){
+  let tracks = [];
+  let url = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks?limit=50`
+  let response = await spotifyApiRequest(url, payload);
+  response = response['items'];
+  for(const track of response){
+    tracks.push(track['track']['name']);
+  }
+  return tracks; // Note: must use await on function call 
+}
+
+async function getGenres(payload = defaultPayload){
+  let genres = [];
+  let url = `https://api.spotify.com/v1/recommendations/available-genre-seeds`
+  let response = await spotifyApiRequest(url, payload);
+  response = response['genres'];
+  for(const genre of response){
+    genres.push(genre);
+  }
+  return genres; // Note: must use await on function call 
+}
+
+async function getUsersTopTracks(user, payload = defaultPayload){
+  let topTracks = [];
+  let url = "https://api.spotify.com/v1/ivzap/top/tracks"
+  let response = await spotifyApiRequest(url, payload);
+  response = response;
+  console.log(response);
+  for(const genre of response){
+    genres.push(genre);
+  }
+  return topTracks; // Note: must use await on function call(endpoint error) 
+}
+
+
+
+
